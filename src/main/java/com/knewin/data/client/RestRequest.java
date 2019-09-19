78a8fcc5717eac35859f89b8165f8606853e8e66
marcio <marcio@knewin.com>
2019-09-19 12:15:29 -0300

    Increase the http socket timeout for waiting for data

--- src/main/java/com/knewin/data/client/RestRequest.java
+++ src/main/java/com/knewin/data/client/RestRequest.java
@@ -2,6 +2,7 @@ package com.knewin.data.client;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.time.Duration;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.client.config.RequestConfig;
@@ -14,6 +15,11 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 
 public class RestRequest {
+
+	private static final Duration DEFAULT_CONNECT_TIMEOUT = Duration.ofSeconds(30);
+
+	private static final Duration DEFAULT_READ_TIMEOUT = Duration.ofMinutes(3);
+
 
 	/**
 	 * Request content using HTTP GET method.
@@ -29,8 +35,10 @@ public class RestRequest {
 	 */
 	@Deprecated
 	public String get(final String url) throws DataRequestException {
-		final RequestConfig requestConfig = RequestConfig.custom().setConnectionRequestTimeout(30000).setSocketTimeout(30000)
-			.setConnectTimeout(30000).build();
+		final RequestConfig requestConfig = RequestConfig.custom()
+			.setConnectionRequestTimeout((int) DEFAULT_CONNECT_TIMEOUT.toMillis())
+			.setSocketTimeout((int) DEFAULT_READ_TIMEOUT.toMillis())
+			.setConnectTimeout((int) DEFAULT_CONNECT_TIMEOUT.toMillis()).build();
 
 		try (final CloseableHttpClient httpClient = HttpClientBuilder.create().setDefaultRequestConfig(requestConfig)
 			.build()) {
@@ -81,8 +89,10 @@ public class RestRequest {
 	 * @throws DataRequestException error in case of problems to request remote content
 	 */
 	protected String post(final String bodyContent, final String url) throws DataRequestException {
-		final RequestConfig requestConfig = RequestConfig.custom().setConnectionRequestTimeout(30000).setSocketTimeout(30000)
-			.setConnectTimeout(30000).build();
+		final RequestConfig requestConfig = RequestConfig.custom()
+			.setConnectionRequestTimeout((int) DEFAULT_CONNECT_TIMEOUT.toMillis())
+			.setSocketTimeout((int) DEFAULT_READ_TIMEOUT.toMillis())
+			.setConnectTimeout((int) DEFAULT_READ_TIMEOUT.toMillis()).build();
 
 		try (final CloseableHttpClient httpClient = HttpClientBuilder.create().setDefaultRequestConfig(requestConfig)
 			.build()) {
